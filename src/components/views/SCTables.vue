3fa1dde889c22b0b7880e138f614d29c1e28f439
liyang <121777824@qq.com>
2017-05-03 21:18:46 +0800

    add tags

--- src/components/views/SCTables.vue
+++ src/components/views/SCTables.vue
@@ -33,26 +33,36 @@
         <el-table-column prop="date" label="日期" width="180" sortable></el-table-column>
         <el-table-column prop="name" label="用户名" width="100"></el-table-column>
         <el-table-column prop="address" label="地址" width="280"></el-table-column>
-        <el-table-column 
-          prop="tag"
+        <el-table-column
+          prop="tags"
           label="标签"
           width="280">
           <template scope="scope">
-            <el-tag 
+            <el-tag
               :key="tag"
-              v-for="tag in dynamicTags"
+              v-for="tag in scope.row.tags"
               :closable="true"
-              :close-transition="false">
-              {{ tag }}
+              :close-transition="false"
+              @close="handleClose(tag,scope.row.tags)"
+            >
+              {{tag}}
+
+
+
             </el-tag>
             <el-input
               class="input-new-tag"
-              v-if="inputVisible"
+              v-if="scope.row.inputVisible"
               v-model="inputValue"
               ref="saveTagInput"
               size="mini"
-              ></el-input>
-              <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
+              @keyup.enter.native="handleInputConfirm(scope.row.id)"
+              @blur="handleInputConfirm(scope.row.id)"
+            ></el-input>
+            <el-button v-else class="button-new-tag" size="small" @click="showInput(scope.row.id)">+ New Tag
+
+
+            </el-button>
           </template>
         </el-table-column>
         <el-table-column label="操作">
@@ -75,71 +85,98 @@
             layout="total,sizes,prev,pager,next,jumper">
           </el-pagination>
         </el-col>
-      </el-row>  
+      </el-row>
     </div>
   </div>
 </template>
 
 <script>
-import faker from 'faker'
-export default {
-  name: 'sc-table',
-  data () {
-    return {
-      options: [{
-        value: '选项1',
-        label: '黄金糕'
-      }, {
-        value: '选项2',
-        label: '双皮奶'
-      }, {
-        value: '选项3',
-        label: '蚵仔煎'
-      }, {
-        value: '选项4',
-        label: '龙须面'
-      }, {
-        value: '选项5',
-        label: '北京烤鸭'
-      }],
-      value: '',
-      input: '',
-      tableData: [{
-        date: '2016-05-02',
-        id: '1',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        id: '2',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        id: '3',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        id: '4',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }],
-      contentTotal: 1000,
-      currentPage: 1,
-      dynamicTags: ['标签一', '标签二', '标签三']
+  import faker from 'faker'
+  export default {
+    name: 'sc-table',
+    data () {
+      return {
+        options: [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+        value: '',
+        input: '',
+        tableData: [{
+          date: '2016-05-02',
+          id: '1',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+          tags: ['标签一', '标签二', '标签三'],
+          inputValue: '',
+          inputVisible: false
+        }, {
+          date: '2016-05-04',
+          id: '2',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄',
+          tags: ['标签4', '标签5', '标签6'],
+          inputValue: '',
+          inputVisible: false
+        }, {
+          date: '2016-05-01',
+          id: '3',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄',
+          tags: ['标签7', '标签8', '标签9'],
+          inputValue: '',
+          inputVisible: false
+        }, {
+          date: '2016-05-03',
+          id: '4',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄',
+          tags: ['标签一', '标签二', '标签三'],
+          inputValue: '',
+          inputVisible: false
+        }],
+        contentTotal: 1000,
+        currentPage: 1
+      }
+    },
+    computed: {},
+    components: {},
+    methods: {
+      handleClose (tag, tagList) {
+        //  todo
+        tagList.splice(tagList.indexOf(tag), 1)
+      },
+      showInput (id) {
+        console.log(this.tableData[parseInt(id) - 1].inputVisible)
+        this.tableData[parseInt(id) - 1].inputVisible = true
+        this.$nextTick(_ => {
+          console.log(this)
+          this.$refs.saveTagInput.$refs.input.focus()
+        })
+      },
+      handleInputConfirm (id) {
+//        let inputValue = this.tableData[parseInt(id) - 1].inputValue
+//        if(inputValue) {
+//          this
+//        }
+      }
+    },
+    mounted () {
+      console.log(faker.fake('{{name.firstName}}, {{name.lastName}}, {{name.suffix}}'))
     }
-  },
-  computed: {
-  },
-  components: {
-  },
-  method: {
-  },
-  mounted: function () {
-    console.log(faker.fake('{{name.firstName}}, {{name.lastName}}, {{name.suffix}}'))
   }
-}
 </script>
 <style scoped>
   .sc-table {
