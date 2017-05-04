7b0558762932d33638c93cd42df3157087772073
3AM <3amhuang@gmail.com>
2017-05-04 13:57:04 +0800

    change sc-table style

--- src/components/views/SCTables.vue
+++ src/components/views/SCTables.vue
@@ -1,37 +1,43 @@
 <template>
   <div class="sc-table">
     <div class="sc-table-header">
-      <el-button class="header-add-button" type="primary">添加</el-button>
-      <el-row type="flex" justify="space-between">
-        <el-col :xs="24" :sm="18" :md="12" :lg="6">
-          <el-row type="flex" justify="space-between">
-            <el-select v-model="value" placeholder="请选择">
-              <el-option
-                v-for="item in options"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-                :disbaled="true">
-              </el-option>
-            </el-select>
-            <el-input v-model="input" placeholder="请输入内容" class="sc-table-header-select"></el-input>
-            <el-button type="primary">搜索</el-button>
+      <el-row type="flex" justify="space-around">
+        <el-col :span="16">
+          <el-row type="flex">
+            <el-col :span="6">
+              <el-select v-model="value" placeholder="请选择">
+                <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                  :disbaled="true">
+                </el-option>
+              </el-select>
+            </el-col>
+            <el-col :span="6">
+              <el-input v-model="input" placeholder="请输入内容" class="sc-table-header-select"></el-input>
+            </el-col>
+            <el-col :span="6">
+              <el-button class="sc-table-search-btn" type="primary">搜索</el-button>
+            </el-col>
           </el-row>
         </el-col>
-        <el-col :xs="24" :sm="6" :md="12" :lg="4">
-          <el-row type="flex" justify="space-around">
-            <el-button>高级搜索</el-button>
-            <el-button>导出</el-button>
-            <el-button>自定义</el-button>
+        <el-col :span="8">
+          <el-row type="flex" justify="end">
+            <el-button type="primary" icon="plus"></el-button>
+            <el-button type="primary" icon="upload2"></el-button>
+            <el-button type="primary" icon="setting"></el-button>
+            <el-button type="primary" icon="search">高级搜索</el-button>
           </el-row>
         </el-col>
       </el-row>
     </div>
     <div class="sc-table-content">
-      <el-table :data="tableData" border :default-sort="{prop: 'date', order: 'descending'}" style="width: 100%;">
+      <el-table :data="tableData" border stripe :default-sort="{prop: 'date', order: 'descending'}" style="width: 100%;">
         <el-table-column type="selection" width="55"></el-table-column>
-        <el-table-column prop="id" label="ID" width="100" sortable></el-table-column>
-        <el-table-column prop="date" label="日期" width="180" sortable></el-table-column>
+        <el-table-column prop="id" label="ID" width="80" sortable></el-table-column>
+        <el-table-column prop="date" label="日期" width="150" sortable></el-table-column>
         <el-table-column prop="name" label="用户名" width="100"></el-table-column>
         <el-table-column prop="address" label="地址" width="280"></el-table-column>
         <el-table-column
@@ -72,15 +78,34 @@
       </el-table>
     </div>
     <div class="sc-table-footer">
-      <el-row :gutter="20">
-        <el-col :span="12" :offset="6">
-          <el-pagination
-            :current-page="currentPage"
-            :page-sizes="[50, 100, 150, 200]"
-            :page-size="50"
-            :total="contentTotal"
-            layout="total,sizes,prev,pager,next,jumper">
-          </el-pagination>
+      <el-row type="flex" justify="center">
+        <el-col>
+          <el-row type="flex">
+            <el-pagination
+              :page-sizes="[20,50,100,200]"
+              :page-size="20"
+              layout="sizes"
+              >
+            </el-pagination>
+          </el-row>
+        </el-col>
+        <el-col>
+          <el-row type="flex" justify="center">
+            <el-pagination
+              :total="contentTotal"
+              :current-page="currentPage"
+              layout="total">
+            </el-pagination>
+          </el-row>
+        </el-col>
+        <el-col>
+          <el-row type="flex" justify="end">
+            <el-pagination
+              :current-page="currentPage"
+              :total="contentTotal"
+              layout="prev, pager, next">
+            </el-pagination>
+          </el-row>
         </el-col>
       </el-row>
     </div>
@@ -88,102 +113,102 @@
 </template>
 
 <script>
-  import faker from 'faker'
-  export default {
-    name: 'sc-table',
-    data () {
-      return {
-        options: [{
-          value: '选项1',
-          label: '黄金糕'
-        }, {
-          value: '选项2',
-          label: '双皮奶'
-        }, {
-          value: '选项3',
-          label: '蚵仔煎'
-        }, {
-          value: '选项4',
-          label: '龙须面'
-        }, {
-          value: '选项5',
-          label: '北京烤鸭'
-        }],
-        value: '',
-        input: '',
-        tableData: [{
-          date: '2016-05-02',
-          id: '1',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄',
-          tags: ['标签一', '标签二', '标签三'],
-          inputValue: '',
-          inputVisible: false
-        }, {
-          date: '2016-05-04',
-          id: '2',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄',
-          tags: ['标签4', '标签5', '标签6'],
-          inputValue: '',
-          inputVisible: false
-        }, {
-          date: '2016-05-01',
-          id: '3',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄',
-          tags: ['标签7', '标签8', '标签9'],
-          inputValue: '',
-          inputVisible: false
-        }, {
-          date: '2016-05-03',
-          id: '4',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄',
-          tags: ['标签一', '标签二', '标签三'],
-          inputValue: '',
-          inputVisible: false
-        }],
-        contentTotal: 1000,
-        currentPage: 1
-      }
-    },
-    computed: {},
-    components: {},
-    methods: {
-      handleClose (tag, tagList) {
-        //  todo
-        tagList.splice(tagList.indexOf(tag), 1)
-      },
-      showInput (id) {
-        this.tableData[parseInt(id) - 1].inputVisible = true
-        this.$nextTick(_ => {
-          this.$refs.saveTagInput.$refs.input.focus()
-        })
-      },
-      handleInputConfirm (id) {
-        let inputValue = this.tableData[parseInt(id) - 1].inputValue
-        if (inputValue) {
-          this.tableData[parseInt(id) - 1].tags.push(inputValue)
-        }
-        this.tableData[parseInt(id) - 1].inputVisible = false
-        this.tableData[parseInt(id) - 1].inputValue = ''
-      }
-    },
-    mounted () {
-      console.log(faker.fake('{{name.firstName}}, {{name.lastName}}, {{name.suffix}}'))
+import faker from 'faker'
+export default {
+  name: 'sc-table',
+  data () {
+    return {
+      options: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }],
+      value: '',
+      input: '',
+      tableData: [{
+        date: '2016-05-02',
+        id: '1',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄',
+        tags: ['标签一', '标签二', '标签三'],
+        inputValue: '',
+        inputVisible: false
+      }, {
+        date: '2016-05-04',
+        id: '2',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄',
+        tags: ['标签4', '标签5', '标签6'],
+        inputValue: '',
+        inputVisible: false
+      }, {
+        date: '2016-05-01',
+        id: '3',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄',
+        tags: ['标签7', '标签8', '标签9'],
+        inputValue: '',
+        inputVisible: false
+      }, {
+        date: '2016-05-03',
+        id: '4',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄',
+        tags: ['标签一', '标签二', '标签三'],
+        inputValue: '',
+        inputVisible: false
+      }],
+      contentTotal: 1000,
+      currentPage: 1
     }
+  },
+  computed: {},
+  components: {},
+  methods: {
+    handleClose (tag, tagList) {
+      //  todo
+      tagList.splice(tagList.indexOf(tag), 1)
+    },
+    showInput (id) {
+      this.tableData[parseInt(id) - 1].inputVisible = true
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus()
+      })
+    },
+    handleInputConfirm (id) {
+      let inputValue = this.tableData[parseInt(id) - 1].inputValue
+      if (inputValue) {
+        this.tableData[parseInt(id) - 1].tags.push(inputValue)
+      }
+      this.tableData[parseInt(id) - 1].inputVisible = false
+      this.tableData[parseInt(id) - 1].inputValue = ''
+    }
+  },
+  mounted () {
+    console.log(faker.fake('{{name.firstName}}, {{name.lastName}}, {{name.suffix}}'))
   }
+}
 </script>
 <style scoped>
-  .sc-table {
-    border-top: 1px solid lightgray;
-    padding-top: 2rem;
-    margin-top: 2rem;
-  }
-
-  .sc-table-header .header-add-button {
-    margin-bottom: 1rem;
-    margin-left: 2rem;
-  }
+.sc-table {
+  border-top: 1px solid lightgray;
+  padding-top: 2rem;
+  margin-left: 2rem;
+  margin-top: 2rem;
+  margin-right: 2rem;
+}
+.sc-table-search-btn {
+  margin-left: 20px;
+}
 </style>
