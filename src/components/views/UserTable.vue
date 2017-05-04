2fa167c8a81658988f3d5506b67c667a3115cb2e
3AM <3amhuang@gmail.com>
2017-05-04 21:48:01 +0800

    user table add remove user function

--- src/components/views/UserTable.vue
+++ src/components/views/UserTable.vue
@@ -1,11 +1,34 @@
 <template>
   <div v-if="response" class="sc-user-table">
+    <el-form v-if="advancedForm" class="search-form" :model="searchForm">
+      <el-form-item label="ID">
+        <el-input :model="advancedForm.id"></el-input>
+      </el-form-item>
+      <el-form-item label="用户名">
+        <el-input :model="advancedForm.username"></el-input>
+      </el-form-item>
+      <el-form-item label="手机号">
+        <el-input :model="advancedForm.username"></el-input>
+      </el-form-item>
+      <el-form-item label="地址">
+        <el-input :model="advancedForm.address"></el-input>
+      </el-form-item>
+      <el-form-item label="创建时间">
+          <el-col :span="11">
+            <el-date-picker type="date" placeholder="选择日期" v-model="searchForm.startDate" style="width: 100%;"></el-date-picker>
+          </el-col>
+          <el-col class="line" :span="2">-</el-col>
+          <el-col :span="11">
+            <el-time-picker type="fixed-time" placeholder="选择时间" v-model="searchForm.endDate" style="width: 100%;"></el-time-picker>
+          </el-col>
+        </el-form-item>
+    </el-form>
     <div class="sc-table-header">
       <el-row type="flex" justify="space-around">
         <el-col :span="16">
           <el-row type="flex">
             <el-col :span="4">
-              <el-select v-model="value" placeholder="请选择">
+              <el-select v-model="searchSelect" placeholder="请选择">
                 <el-option
                   v-for="item in options"
                   :key="item.value"
@@ -16,11 +39,11 @@
               </el-select>
             </el-col>
             <el-col :span="6">
-              <el-input v-model="input" placeholder="请输入内容" class="sc-table-header-select"></el-input>
+              <el-input v-model="searchInput" placeholder="请输入内容" class="sc-table-header-select"></el-input>
             </el-col>
             <el-col :span="8">
-              <el-button class="sc-table-search-btn" type="primary">搜索</el-button>
-              <el-button type="primary" icon="search">高级搜索</el-button>
+              <el-button @click="onSingleSearch" class="sc-table-search-btn" type="primary">搜索</el-button>
+              <el-button @click="onAdvancedSearch" type="primary" icon="search">高级搜索</el-button>
             </el-col>
           </el-row>
         </el-col>
@@ -34,11 +57,11 @@
       </el-row>
     </div>
     <div class="sc-table-content">
-      <el-table :data="response.data" border stripe :default-sort="{prop: 'date', order: 'descending'}" style="width: 100%;">
+      <el-table :data="response.data" border stripe :default-sort="{prop: 'date', order: 'descending'}">
         <el-table-column type="selection" width="55"></el-table-column>
         <el-table-column prop="id" label="ID" width="80" sortable></el-table-column>
-        <el-table-column prop="createdAt" label="创建日期" width="150" sortable></el-table-column>
         <el-table-column prop="username" label="用户名" width="100"></el-table-column>
+        <el-table-column prop="phone" label="手机号码" width="150" sortable></el-table-column>
         <el-table-column prop="email" label="Email" width="190"></el-table-column>
         <el-table-column prop="address" label="地址" width="200"></el-table-column>
         <el-table-column
@@ -61,18 +84,17 @@
               ref="saveTagInput"
               size="mini"
               @keyup.enter.native="handleInputConfirm(scope.row.id)"
-              @blur="handleInputConfirm(scope.row.id)"
-            ></el-input>
-            <el-button v-else class="button-new-tag" size="small" @click="showInput(scope.row.id)">+ New Tag
-
+              @blur="handleInputConfirm(scope.row.id)">
+            </el-input>
+            <el-button v-else class="button-new-tag" size="small" @click="showInput(scope.row.id)">添加标签
             </el-button>
           </template>
         </el-table-column>
         <el-table-column label="操作">
           <template scope="scope">
-            <el-button size="small">详情</el-button>
-            <el-button size="small">修改</el-button>
-            <el-button size="small" type="danger">删除</el-button>
+            <el-button size="small" icon="document"></el-button>
+            <el-button size="small" icon="edit"></el-button>
+            <el-button @click="onDeleteUser(scope.row.id)" size="small" type="danger" icon="delete2"></el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -100,10 +122,20 @@ export default {
   name: 'sc-user-table',
   data () {
     return {
-      value: '',
-      input: '',
-      contentTotal: 1000,
+      searchSelect: '',
+      searchInput: '',
+      advancedForm: false,
+      searchForm: {
+        id: '',
+        username: '',
+        phone: '',
+        startDate: '',
+        endDate: '',
+        address: '',
+        createdAt: ''
+      },
       userListURL: 'http://192.168.1.2:8080/admin/user/index',
+      removeUserURL: 'http://192.168.1.2:8080/admin/user/delete',
       response: null,
       options: [],
       error: null
@@ -131,8 +163,44 @@ export default {
       this.tableData[parseInt(id) - 1].inputVisible = false
       this.tableData[parseInt(id) - 1].inputValue = ''
     },
+    onSingleSearch () {
+      if (this.searchSelect === '') {
+        return
+      } else if (this.searchInput === '') {
+        return
+      }
+
+      // TODO search user
+    },
+    onAdvancedSearch () {
+      console.log('111')
+      this.advancedForm = !this.advancedForm
+    },
+    onDeleteUser (id) {
+      this.$confirm('此操作将删除, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        // delete user
+        axios.post(this.removeUserURL, {
+          id: id
+        })
+        .then(response => {
+          console.log(response.data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+    },
     getUserLists () {
-      axios.get('http://192.168.1.2:8080/admin/user/index')
+      axios.get(this.userListURL)
       .then(response => {
         console.log(response)
 
@@ -156,7 +224,7 @@ export default {
     }
   },
   mounted () {
-    console.log('222')
+    console.log(this.$store)
     this.getUserLists()
   }
 }
