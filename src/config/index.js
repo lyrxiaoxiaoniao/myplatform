d2de91596785642f0756927f2bbfbb614367765d
liyang <121777824@qq.com>
2017-05-06 11:08:23 +0800

    add routes and modify form-style

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
-  serverURI: 'http://192.168.1.2:8080',
+  serverURI: 'http://192.168.1.5:8080',
   appInfoAPI: '/admin/option/show',
   userListAPI: '/admin/user/index',
   removeUserAPI: '/admin/user/delete',
--- src/routes.js
+++ src/routes.js
@@ -17,6 +17,7 @@ import SCUserTableView from './components/views/UserTable.vue'
 import SCReportTableView from './components/views/ReportTable'
 import SCUserAddFormView from './components/views/UserAddForm'
 import SCReportDeatilView from './components/views/ReportDetail'
+import SCReportDeal from './components/views/ReportDeal.vue'
 
 // Routes
 const routes = [
@@ -89,6 +90,11 @@ const routes = [
         component: SCReportDeatilView,
         name: '案件详情',
         meta: {description: '案件详情列表'}
+      }, {
+        path: 'reportdeal',
+        component: SCReportDeal,
+        name: '案件处理',
+        meta: {description: '案件处理'}
       }
     ]
   }, {
