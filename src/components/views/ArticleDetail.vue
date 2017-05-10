d15bb3479ab17cf9697aae4b635dabe92a864365
liyang <liyang@shencom.cn>
2017-05-10 16:33:09 +0800

    fix article-add-form-style article-detail-style

--- src/components/views/ArticleDetail.vue
+++ src/components/views/ArticleDetail.vue
@@ -1,38 +1,35 @@
 <template>
   <div class="articleDetail">
     <el-row type="flex" justify="center">
-      <el-col :span="18">
+      <el-col :span="15" class="articleDetailPanel">
         <el-row>
-          <el-col :span="12">
-            <h3>1234</h3>
+          <h2>{{ article.title }}</h2>
+        </el-row>
+        <el-row>
+          <el-col :span="18">
+            发布时间:{{ article.time }} 作者:{{ article.author }} 浏览次数:{{ article.views }}
           </el-col>
-          <el-col :span="12">
-            <el-button type="primary">通过审查</el-button>
-            <el-button>编辑</el-button>
-            <el-button type="danger">删除</el-button>
+          <el-col :span="6">
+            <el-checkbox>通过审查</el-checkbox>
+            <el-button icon="edit" size="mini" type="primary">编辑</el-button>
+            <el-button icon="delete" size="mini" type="danger">删除</el-button>
           </el-col>
         </el-row>
         <el-row>
-          发布时间: 123456 123456  作者： 21312312  浏览次数： 13123123
+          {{ article.summary }}
         </el-row>
-        <el-row>
-          213213123123123123123123123
+        <el-row type="flex" justify="center">
+          {{ article.content }}
         </el-row>
-        <el-row>
-          <el-button type="primary" icon="caret-top"></el-button>
+        <el-row type="flex" justify="end">
+          <el-button icon="caret-top" size="mini">{{ article.star }}</el-button>
           <el-checkbox>推荐</el-checkbox>
           <el-checkbox>置顶</el-checkbox>
           <el-checkbox>热点</el-checkbox>
-          <el-button icon="document">评论</el-button>
         </el-row>
-        <el-row>
-          <p>评论</p>
-          <vue-html5-editor :content="article.comment" @change="updateData"></vue-html5-editor>
-          <el-checkbox>匿名评论</el-checkbox>
-          <el-button>发布评论</el-button>
-        </el-row>
-        <el-row>
-          <p>热门评论</p>
+        <el-row type="flex" justify="space-between">
+          <span><el-button>上一篇</el-button>{{ article.pre }}</span>
+          <span>{{ article.next }}<el-button>下一篇</el-button></span>
         </el-row>
       </el-col>
     </el-row>
@@ -43,8 +40,16 @@
     data () {
       return {
         article: {
-          title: '',
-          comment: ''
+          title: '1234',
+          time: '2016',
+          author: '江鹏',
+          views: '1234',
+          summary: '摘要',
+          content: '正文',
+          star: 20,
+          top: true,
+          pre: '123',
+          next: '345'
         }
       }
     },
@@ -60,5 +65,10 @@
     margin-top: 2rem;
     padding-top: 2rem;
     border-top: 1px solid lightgray;
+  }
+  .articleDetailPanel {
+    padding: 2rem;
+    background-color: white;
+    box-shadow: 1px 1px 1px 1px lightgrey;
   }
 </style>
