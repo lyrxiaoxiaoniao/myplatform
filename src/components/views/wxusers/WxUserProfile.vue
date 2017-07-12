aec5843306de24347494404f88c3ad74498efa4a
3AM <3amhuang@gmail.com>
2017-07-12 10:12:29 +0800

    Delete Serveral Information In Wechat Profile Component

--- src/components/views/wxusers/WxUserProfile.vue
+++ src/components/views/wxusers/WxUserProfile.vue
@@ -8,7 +8,7 @@
               <kobe-info-cell title="姓名" :info="response.realname ? response.realname : '无'"></kobe-info-cell>
               <kobe-info-cell title="微信昵称" :info="response.nickname ? response.nickname : '无'"></kobe-info-cell>
               <kobe-info-cell title="手机" :info="response.mobile ? response.mobile : '无'"></kobe-info-cell>
-              <kobe-info-cell title="职位" :info="response.merchant ? response.merchant.name : '无'"></kobe-info-cell>
+              <kobe-info-cell title="职位" :info="response.merchant.name ? response.merchant.name : '无'"></kobe-info-cell>
               <kobe-info-cell>
                 <div slot="kobe-cell-content">
                   <el-row type="flex" justify="end">
@@ -41,21 +41,11 @@
               <div>企业地址: {{ response.fCompanyInfo.address }}</div>
               <div>企业人数: {{ response.fCompanyInfo.count }}人</div>
               <div>所属行业: {{ response.fCompanyInfo.industryName }}</div>
-              <div>安全证书编号: {{ response.fCompanyInfo.safecert }}</div>
               <div>所属街道: {{ response.fCompanyInfo.subDistrictName }}/{{ response.fCompanyInfo.community }}</div>
-            </el-card>
-            <el-card class="sc-wechat-company-card">
-              <div class="sc-wechat-company-card-title" slot="header">
-                <span>组织信息</span>
-              </div>
-              <div>组织名称: {{ response.fCompanyInfo.orgaName }}</div>
-              <div>统一信用代码: {{ response.fCompanyInfo.orgaCode }}</div>
-              <div>详细地址: {{ response.fCompanyInfo.orgaAddress }}</div>
             </el-card>
           </el-col>
           <el-col :span="6" class="sc-wechat-profile-image-content">
-            <kobe-image-pane v-if="response.fCompanyInfo.orgaPhotoUrl" :image="response.fCompanyInfo.orgaPhotoUrl" description="组织机构代码证"></kobe-image-pane>
-            <kobe-image-pane v-if="response.fCompanyInfo.safecertUrl" :image="response.fCompanyInfo.safecertUrl" description="企业主要负责人安全培训合格证书"></kobe-image-pane>
+            <kobe-image-pane v-if="response.fCompanyInfo.licenseUrl" :image="response.fCompanyInfo.licenseUrl" description="企业营业执照"></kobe-image-pane>
           </el-col>
         </el-row>
       </el-tab-pane>
