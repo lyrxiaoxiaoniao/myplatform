fcbad5d60bf88e182750a1e8fb2618be3547a228
liuyouren <liuyouren@shencom.cn>
2017-08-08 19:34:04 +0800

    modify test URL

--- config/index.js
+++ config/index.js
@@ -7,8 +7,8 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    // assetsPublicPath: './',
-    assetsPublicPath: 'https://scplugins.oss-cn-shenzhen.aliyuncs.com/plugins/gm-admin/1.0.0/',
+    assetsPublicPath: './',
+    // assetsPublicPath: 'https://scplugins.oss-cn-shenzhen.aliyuncs.com/plugins/gm-admin/1.0.0/',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
